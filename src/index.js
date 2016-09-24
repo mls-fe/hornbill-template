4110f70ac8284b8c7efff1ab0dfd89136c9d928e
sunnylost <sunnylost@gmail.com>
2016-09-24 23:19:55 +0800

    change Template to class object, which extends from EventEmitter; catch error, provide error event;

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
 'use strict'
 
-let Path     = require( 'path' ),
-    FS       = require( 'fs' ),
-    Compiler = require( './compiler' ),
-    Ext      = require( './ext' ),
-    cache    = new Map(),
-    Template
+let Path          = require( 'path' ),
+    FS            = require( 'fs' ),
+    EventEmitter  = require( 'events' ),
+    Compiler      = require( './compiler' ),
+    Ext           = require( './ext' ),
+    cache         = new Map(),
+    defaultConfig = {
+        errorHandler( err ) {
+            /* eslint-disable */
+            console.error( err )
+            /* eslint-enable */
+        }
+    }
 
-Template = {
-    compile( codes ) {
-        let sourceCode = Compiler.compile( codes )
+class Template extends EventEmitter {
+    constructor( config ) {
+        super()
+        this._config = Object.assign( {}, defaultConfig, config )
+    }
+
+    compile( codes, config ) {
+        let sourceCode = Compiler.compile( codes, config )
         return new Function( sourceCode )
-    },
+    }
 
     render( filepath, data, callback ) {
         filepath = Path.resolve( filepath )
 
-        let fn
+        let config = this._config,
+            fn
 
         if ( cache.has( filepath ) ) {
             fn = cache.get( filepath )
@@ -26,7 +39,7 @@ Template = {
                     encoding: 'utf8'
                 } )
 
-                fn = this.compile( codes )
+                fn = this.compile( codes, this._config )
                 cache.set( filepath, fn )
             } catch ( e ) {
                 /* eslint-disable */
@@ -36,24 +49,39 @@ Template = {
             }
         }
 
-        typeof callback === 'function' && callback( fn.call( Object.assign( {}, data, {
-            [ Compiler.EXT_OBJECT ] : Ext,
-            [ Compiler.TE_OBJECT ]  : Template,
-            [ Compiler.PATH_OBJECT ]: Path,
-            [ Compiler.DIR_OBJECT ] : Path.dirname( filepath )
-        } ) ) )
-    },
+        try {
+            typeof callback === 'function' && callback( fn.call( Object.assign( {}, data, {
+                [ Compiler.EXT_OBJECT ] : Ext,
+                [ Compiler.TE_OBJECT ]  : Template,
+                [ Compiler.PATH_OBJECT ]: Path,
+                [ Compiler.DIR_OBJECT ] : config.basePath || Path.dirname( filepath )
+            } ) ) )
+        } catch ( e ) {
+            this.emit( 'error', e )
+        }
+
+        return this
+    }
 
     renderString( codes, data, callback ) {
-        let fn = this.compile( codes )
+        let config = this._config, /**/
+            fn     = this.compile( codes )
 
-        typeof callback === 'function' && callback( fn.call( Object.assign( {}, data, {
-            [ Compiler.EXT_OBJECT ] : Ext,
-            [ Compiler.TE_OBJECT ]  : Template,
-            [ Compiler.PATH_OBJECT ]: Path,
-            [ Compiler.DIR_OBJECT ] : __dirname
-        } ) ) )
+        try {
+            typeof callback === 'function' && callback( fn.call( Object.assign( {}, data, {
+                [ Compiler.EXT_OBJECT ] : Ext,
+                [ Compiler.TE_OBJECT ]  : Template,
+                [ Compiler.PATH_OBJECT ]: Path,
+                [ Compiler.DIR_OBJECT ] : config.basePath || __dirname
+            } ) ) )
+        } catch ( e ) {
+            this.emit( 'error', e )
+        }
+
+        return this
     }
 }
 
-module.exports = Template
+module.exports = ( config ) => {
+    return new Template( config )
+}
